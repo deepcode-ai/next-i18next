520137e67c800fa373fa679ae70cd751065ce8d0
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-12-01 07:43:40 +0600

    init commit

--- src/appWithTranslation.tsx
+++ src/appWithTranslation.tsx
@@ -1,42 +1,38 @@
-import React, { useMemo, useRef } from 'react'
+import React, { useMemo } from 'react'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import { I18nextProvider } from 'react-i18next'
-import type { AppProps as NextJsAppProps } from 'next/app'
 
 import { createConfig } from './config/createConfig'
 import createClient from './createClient'
 
 import { SSRConfig, UserConfig } from './types'
 
+import { i18nContainer } from './globali18n'
+
 import { i18n as I18NextClient } from 'i18next'
-import { useIsomorphicLayoutEffect } from './utils'
 export {
   Trans,
   useTranslation,
   withTranslation,
 } from 'react-i18next'
 
-export let globalI18n: I18NextClient | null = null
-
-export const appWithTranslation = <Props extends NextJsAppProps>(
+export const appWithTranslation = <
+  Props extends Record<string, unknown>
+>(
   WrappedComponent: React.ComponentType<Props>,
   configOverride: UserConfig | null = null
 ) => {
-  const AppWithTranslation = (
-    props: Props & { pageProps: Props['pageProps'] & SSRConfig }
-  ) => {
-    const { _nextI18Next } = props.pageProps || {} // pageProps may be undefined on strange setups, i.e. https://github.com/i18next/next-i18next/issues/2109
+  const AppWithTranslation = (props: Props & SSRConfig) => {
+    const { _nextI18Next } = props || {} // pageProps may be undefined on strange setups, i.e. https://github.com/i18next/next-i18next/issues/2109
     let locale: string | undefined =
-      _nextI18Next?.initialLocale ?? props?.router?.locale
+      _nextI18Next?.initialLocale;
+      
     const ns = _nextI18Next?.ns
 
-    const instanceRef = useRef<I18NextClient | null>(null)
-
-    /**
-     * Memoize i18n instance and reuse it rather than creating new instance.
-     * When the locale or resources are changed after instance was created,
-     * we will update the instance by calling addResourceBundle method on it.
-     */
+    // Memoize the instance and only re-initialize when either:
+    // 1. The route changes (non-shallowly)
+    // 2. Router locale changes
+    // 3. UserConfig override changes
     const i18n: I18NextClient | null = useMemo(() => {
       if (!_nextI18Next && !configOverride) return null
 
@@ -67,47 +63,20 @@ export const appWithTranslation = <Props extends NextJsAppProps>(
 
       if (!locale) locale = userConfig.i18n.defaultLocale
 
-      let instance = instanceRef.current
-      if (instance) {
-        if (resources) {
-          for (const locale of Object.keys(resources)) {
-            for (const ns of Object.keys(resources[locale])) {
-              instance.addResourceBundle(
-                locale,
-                ns,
-                resources[locale][ns],
-                true,
-                true
-              )
-            }
-          }
-        }
-      } else {
-        instance = createClient({
-          ...createConfig({
-            ...userConfig,
-            lng: locale,
-          }),
+      const instance = createClient({
+        ...createConfig({
+          ...userConfig,
           lng: locale,
-          ns,
-          resources,
-        }).i18n
+        }),
+        lng: locale,
+        ns,
+        resources,
+      }).i18n
 
-        globalI18n = instance
-        instanceRef.current = instance
-      }
+      i18nContainer.set(() => instance)
 
       return instance
-    }, [_nextI18Next, locale, ns])
-
-    /**
-     * Since calling changeLanguage method on existing i18n instance cause state update in react,
-     * we need to call the method in `useLayoutEffect` to prevent state update in render phase.
-     */
-    useIsomorphicLayoutEffect(() => {
-      if (!i18n || !locale) return
-      i18n.changeLanguage(locale)
-    }, [i18n, locale])
+    }, [_nextI18Next, locale, configOverride, ns])
 
     return i18n !== null ? (
       <I18nextProvider i18n={i18n}>
